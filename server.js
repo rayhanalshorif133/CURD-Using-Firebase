560a39b65b8e470520238b0f6cc2d44c23efa742
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-05-22 11:07:15 +0600

    added routes and others

---
+++ server.js
@@ -0,0 +1,29 @@
+// Initialize
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+
+// dependencies
+const APIRoutes = require('./routes/apiRoutes');
+
+// Static files
+app.use(express.static(__dirname + '/public'));
+
+// configuration
+app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3001;
+
+
+// Routes
+
+app.use('/api', APIRoutes);
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' , port: PORT});
+});
+
+
+// Listen on port 3000
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
